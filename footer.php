a9ae61fcf579b5096af1c355ce92d466a907f43e
Eng. Alphonce Oywech <43148528+Alphonce4305@users.noreply.github.com>
2019-11-08 15:10:38 +0300

    Tunapanda Landing Page
    Please make sure you upload fontawesome and themify-icons for better UI

---
+++ footer.php
@@ -0,0 +1,52 @@
+<br>
+<div class="footer">
+<div class="clear"></div>
+<footer>
+	<br><br>
+	
+	<article>
+		<h5>Quick Links</h5>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">School</a></li>
+			<li><a href="">Hospitals</a></li>
+		</ul>
+	</article>
+	<small class="copyright"> Copyright &copy; TunapandaNet</small>
+</footer>
+</div> 
+<!-- bootstrap js plugins -->
+<script src="plugins/bootstrap-4.0.0/js/bootstrap.js"></script>
+<script src="plugins/bootstrap-4.0.0/js/bootstrap.min.js"></script>
+<!-- CDN JAVASCRIPT LINKS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+</body>
+</html>
+<style type="text/css">
+	.footer{
+		background: grey;
+		width: auto;
+		height: 150px;
+		margin-bottom: 0px;
+		text-align: center;
+		
+	}
+	article{
+		text-align: center;
+		text-transform: uppercase;
+		font-weight: bold;
+		color: white;
+		font-size: 12px;
+	}
+	article ,li ,a{
+		text-align: center;
+		text-transform: uppercase;
+		font-weight: bold;
+		color: blue;
+	}
+	.copyright{
+		text-align: center;
+		margin-top: 10px;
+	}
+</style>
